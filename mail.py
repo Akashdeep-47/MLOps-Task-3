88ab8a3a5dfc45dd94e7d45de7d5630e50691a99
Akashdeep-47 <61601588+Akashdeep-47@users.noreply.github.com>
2020-05-26 18:59:56 +0530

    updating mail.py

--- mail.py
+++ mail.py
@@ -1,22 +1,15 @@
-
 # Python code to illustrate Sending mail from
 # your Gmail account
 import smtplib
-
 # creates SMTP session
 s = smtplib.SMTP('smtp.gmail.com', 587)
-
 # start TLS for security
 s.starttls()
-
 # Authentication
-s.login("akashdeepg06@gmail.com", "guptadeep02")
-
+s.login("sender_email_id", "sender_email_id_password")
 # message to be sent
-message = "Model was successfully trained with highest accuracy achieved"
-
+message = "Message_you_need_to_send"
 # sending the mail
-s.sendmail("akashdeepg06@gmail.com", "akashdeepg196@gmail.com", message)
-
+s.sendmail("sender_email_id", "receiver_email_id", message)
 # terminating the session
 s.quit()
